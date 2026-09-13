1d11d92957eca7d4cc67b687dcc91b4d456e34e7
AzizFatkhuRohman <azizfatkhurohman@outlook.com>
2026-09-13 19:10:31 +0700

    Changes to be committed:

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,22 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\IndexController;
+use App\Http\Controllers\MenuController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('login', [AuthController::class, 'index']);
 Route::post('login', [AuthController::class, 'login'])->name('login');
 Route::middleware('auth')->group(function () {
-    Route::get('/',[IndexController::class,'index']);
+    Route::get('/', [IndexController::class, 'index']);
+    Route::prefix('settings')->group(function () {
+        Route::prefix('menus')->group(function () {
+            Route::get('/', [MenuController::class, 'index']);
+        });
+        Route::prefix('users')->group(function () {
+            Route::get('user', [UserController::class, 'index']);
+            Route::get('role', [RoleController::class, 'index']);
+        });
+    });
 });
